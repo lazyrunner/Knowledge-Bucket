649ad8bbd5f146aa4329602e5a3aebaea1c446f1
Sudeshna Bora <sudeshnamani1@gmail.com>
2021-05-23 19:52:30 +0530

    Add Java code

---
+++ Java/codes/src/collections_main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class collections_main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting the Collection Framework");
+        
+    }
+}
